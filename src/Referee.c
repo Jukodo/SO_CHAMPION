32c965db5ac17aaff29617991cd0e5f6995e7cef
Ricardo <R_P_1997@hotmail.com>
2020-11-07 14:38:25 +0000

    Referee: Get environment variables & handle them

--- src/Referee.c
+++ src/Referee.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "utils/utils.h"
+
+#define DEFAULT_GAMEDIR "~/Documents/SO/SO_CHAMPION/Games/"
+#define DEFAULT_MAXPLAYER 10
 
 void printVariables(int argc, char **argv){
   printf("Argument variables:\n");
@@ -10,8 +14,20 @@ void printVariables(int argc, char **argv){
     printf("\t[%d] - %s\n", i, argv[i]);
   }
 
+  char gamedir[STRING_MEDIUM];
+  strcpy(gamedir, (Utils_StringIsEmpty(getenv("GAMEDIR"))) ?
+    DEFAULT_GAMEDIR :
+    getenv("GAMEDIR"));
+
+  int maxplayer = (Utils_StringIsEmpty(getenv("MAXPLAYER"))) ? 
+    DEFAULT_MAXPLAYER : 
+    Utils_StringIsNumber(getenv("MAXPLAYER")) ? 
+      atoi(getenv("MAXPLAYER")) : 
+      DEFAULT_MAXPLAYER;
+
   printf("\nEnvironment variables:\n");
-  printf("\t[GAMEDIR] - %s\n", getenv("GAMEDIR"));
+  printf("\t[GAMEDIR] - %s\n", gamedir);
+  printf("\t[GAMEDIR] - %d\n", maxplayer);
 }
 
 int main(int argc, char **argv){
--- src/utils/utils.h
+++ src/utils/utils.h
@@ -1,6 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <ctype.h>
+
 //bool define
 typedef int bool;
 #define true 1
 #define false 0
+#define null 0
+
+#define STRING_SMALL 25
+#define STRING_MEDIUM 50
+#define STRING_LARGE 100
+#define STRING_XL 255
 
 void Utils_CleanStdin();
+void Utils_CleanString(char* str);
+bool Utils_StringIsNumber(char* str);
+bool Utils_StringIsEmpty(char* str);
+void Utils_GenerateNewRandSeed();
+int Utils_GetRandomNumber(int min, int max);
