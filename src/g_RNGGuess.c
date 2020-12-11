aba46890c403ef8ffadc1830c338831dd046c49b
Ricardo <R_P_1997@hotmail.com>
2020-12-11 21:12:08 +0000

    RNGGuess return score on SIGUSR1

--- src/g_RNGGuess.c
+++ src/g_RNGGuess.c
@@ -1,26 +1,28 @@
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <string.h>
-#include <signal.h>
+#include <time.h>
 #include <unistd.h>
 
 #include "utils/utils.h"
 
 #define GAME_TITLE "RNGGuess"
 
-void handle_sigusr1(int sig){
+int score = 0;
+void handle_sigusr1(int sig) {
   printf("Received signal: %d AKA SIGUSR1", sig);
+  exit(score);
 }
 
-void Setup_SIGUSR1(){
-  struct sigaction sa = {0}; //{0} - The struct starts with 0's
+void Setup_SIGUSR1() {
+  struct sigaction sa = {0};  //{0} - The struct starts with 0's
   sa.sa_flags = SA_RESTART;
   sa.sa_handler = &handle_sigusr1;
   sigaction(SIGUSR1, &sa, NULL);
 }
 
-void Print_Introduction(){
+void Print_Introduction() {
   printf("\nHello! Welcome to the %s!", GAME_TITLE);
   printf("\nThe game flow is: ");
   printf("\n\t - A random number will be generated from 1 to 9");
@@ -30,51 +32,50 @@ void Print_Introduction(){
   printf("\n\t - The number only changes when you guess it");
 }
 
-int main(int argc, char **argv){
+int main(int argc, char **argv) {
   Setup_SIGUSR1();
 
   sleep(5);
   kill(getpid(), SIGUSR1);
 
   Print_Introduction();
-  
+
   char line[STRING_MEDIUM];
-  
+
   printf("\n\tPlease press ENTER when you are ready to start!");
   getchar();
 
   bool flag_leave = true;
-  int score = 0;
 
-  while(true){
+  while (true) {
     srand(time(0));
-    int number = (rand()%9)+1;
+    int number = (rand() % 9) + 1;
     printf("\nCurrent score: %d\n", score);
-    while(true){
+    while (true) {
       printf("\n\t-> ");
       scanf("%[^\n]", line);
       Utils_CleanStdin();
 
-      if(atoi(line) == number){
+      if (atoi(line) == number) {
         score++;
 
         printf("\tYou got it!\n\tPlay again? (n/N to leave):");
         printf("\n\t-> ");
-        
+
         scanf("%[^\n]", line);
         Utils_CleanStdin();
-        
+
         flag_leave = strcmp(line, "n") == 0 || strcmp(line, "N") == 0;
         break;
-      }else{
+      } else {
         printf("\tWrong...\n");
-        if(abs(atoi(line) - number) <= 2){
+        if (abs(atoi(line) - number) <= 2) {
           printf("\tPretty close though!\n");
         }
       }
     }
 
-    if(flag_leave){
+    if (flag_leave) {
       break;
     }
   }
